ad520d3904f980a73ebb7f8f98ce8f0b2633b24c
zyxl5 <137322909+zyxl5@users.noreply.github.com>
2025-12-07 05:24:33 -0700

    Create ImageStorage.java
    aaa

---
+++ ImageStorage.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class ImageStorage {
+
+    public static ImageIcon load(String path) {
+        return new ImageIcon(path);  // loads images/scene1.png etc.
+    }
+}
